99eec77a94c23cbe131f29aa7dc9a8387bc51298
Mark Wotton <mwotton@gmail.com>
2018-08-05 13:26:51 -0400

    skeleton

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,24 @@
 {-# LANGUAGE LambdaCase #-}
 module Main where
 
-import           Net.Cacher         (cachedFetch)
+import           Control.Monad      (when)
+import           Net.Cacher         (cachedFetch, setupFetcher, validateURLs)
 import           System.Environment (getArgs)
 import           System.Exit        (exitFailure)
+
+abort s = putStrLn s >> exitFailure
+
+handleWarnings errHeader (errors,res) = do
+  when (not $ null errors) $ putStrLn (unlines (errHeader:map show errors))
+  pure res
+
+handleError errHeader = either (abort . show) pure
+
 
 main :: IO ()
 main = do
   getArgs >>= \case
-    [dbFile,urlFile] -> error "not yet"
-    _ -> putStrLn "need to pass db file and url list as arguments" >> exitFailure
+    [dbFile,urlFile, fileDir] -> handleWarnings "Fetch problems:"  =<< goFetch urlFile =<< handleError "setup problem" =<< setupFetcher dbFile fileDir
+    _                         -> abort "need to pass db file and url list as arguments"
+
+goFetch file fetcher = fetcher =<< handleWarnings "Bad urls:" . validateURLs . lines =<< readFile file
--- src/Net/Cacher.hs
+++ src/Net/Cacher.hs
@@ -1,4 +1,28 @@
 module Net.Cacher where
 
-cachedFetch :: FilePath -> [String] -> IO ()
+import           Database.SQLite3
+
+
+data SetupError
+  = NoDBFile FilePath
+  | NoTargetDir FilePath
+  deriving (Show)
+
+data FetchError = FetchError
+  deriving (Show)
+type URL=String -- fixme
+
+setupFetcher :: FilePath -> FilePath -> IO (Either SetupError ([String] -> IO ([FetchError], ())))
+setupFetcher = undefined
+
+-- data Fetcher
+--   = Fetcher
+--   { db :: Database
+--   , targetDir :: FilePath
+--   }
+
+cachedFetch :: FilePath -> [String] -> IO (Either [FetchError] ())
 cachedFetch = undefined
+
+validateURLs :: [String] -> ([String], [URL])
+validateURLs = undefined
