47ef71259e0ef1a377e3fd5ee9d8186c3dbfc7a5
Mark Wotton <mwotton@gmail.com>
2018-08-05 11:32:06 -0400

    reorg

---
+++ app/Main.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE LambdaCase #-}
+module Main where
+
+import           Net.Cacher         (cachedFetch)
+import           System.Environment (getArgs)
+import           System.Exit        ()
+
+main :: IO ()
+main = do
+  getArgs >>= \case
+    [dbFile,urlFile] -> error "not yet"
+    _ -> putStrLn "need to pass db file and url list as arguments" >> exitFailure
